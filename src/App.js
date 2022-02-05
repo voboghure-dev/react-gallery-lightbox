0d37e9ba1a8dad039e881946ed757adf716f26eb
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-02-06 00:18:54 +0600

    Add images in public folder
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+
+const images = [
+	{ id: '1', imageName: 'img1.jpeg', tag: 'free' },
+	{ id: '2', imageName: 'img2.jpeg', tag: 'new' },
+	{ id: '3', imageName: 'img3.jpeg', tag: 'pro' },
+	{ id: '4', imageName: 'img4.jpeg', tag: 'pro' },
+	{ id: '5', imageName: 'img5.jpeg', tag: 'free' },
+	{ id: '6', imageName: 'img6.jpeg', tag: 'new' },
+	{ id: '7', imageName: 'img6.jpeg', tag: 'pro' },
+	{ id: '8', imageName: 'img8.jpeg', tag: 'free' },
+	{ id: '9', imageName: 'img9.jpeg', tag: 'new' },
+	{ id: '10', imageName: 'img10.jpeg', tag: 'new' },
+	{ id: '11', imageName: 'img11.jpeg', tag: 'new' },
+	{ id: '12', imageName: 'img12.jpeg', tag: 'new' },
+	{ id: '13', imageName: 'img13.jpeg', tag: 'free' },
+	{ id: '14', imageName: 'img14.jpeg', tag: 'pro' },
+	{ id: '15', imageName: 'img15.jpeg', tag: 'free' },
+	{ id: '16', imageName: 'img16.jpeg', tag: 'new' }
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hell React</h1>
     </div>
   );
 }
