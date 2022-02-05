e4b41eff87000075c68cffcb69cd6b8193f741f6
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-02-06 02:32:33 +0600

    Try simple react lightbox but fails
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import SimpleReactLightbox from 'simple-react-lightbox';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <SimpleReactLightbox> */}
+      <App />
+    {/* </SimpleReactLightbox> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
